34da94a47a3e16ada6731189f2636690c268f4e3
eliangerard <eliangerardgg@gmail.com>
2024-04-22 22:36:48 -0600

    Delete post options

--- src/app/components/post-card.tsx
+++ src/app/components/post-card.tsx
@@ -14,32 +14,21 @@ export default function PostCard ({
 }
 ) {
   return (
-    <Card className="bg-transparent hover:bg-slate-800 transition border-b rounded-none cursor-pointer border-white/20">
+    <Card className="bg-transparent border-[1px] border-white hover:bg-white/[0.02] transition rounded-2xl cursor-pointer border-white/20 m-2 p-2">
       <CardHeader className="justify-between">
-        <div className="flex gap-5">
-          <Avatar isBordered radius="full" size="md" src={avatarUrl} />
-          <div className="flex flex-col gap-1 items-start justify-center">
+        <div className="flex gap-2">
+          <Avatar isBordered radius="full" size="sm" src={avatarUrl} />
+          <div className="flex flex-col items-start justify-center">
             <h4 className="text-small font-semibold leading-none text-default-600">{userFullName}</h4>
             <h5 className="text-small tracking-tight text-default-400">{'@' + userName}</h5>
           </div>
         </div>
       </CardHeader>
-      <CardBody className="px-3 py-0 text-xs text-default-400">
-        <p className="text-white">
+      <CardBody className="px-3 py-0 text-xs text-default-400 mb-2">
+        <p className="text-white text-md">
           {content}
         </p>
       </CardBody>
-      <CardFooter className="gap-3">
-        <button>
-          <IconMessageCircle className="w-4 h-4" />
-        </button>
-        <button>
-          <IconHeartCode className="w-4 h-4" />
-        </button>
-        <button>
-          <IconRepeat className="w-4 h-4" />
-        </button>
-      </CardFooter>
     </Card>
   )
 }
