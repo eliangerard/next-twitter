e18ac252aa6c5d0e4e50f231870d22d652d65762
eliangerard <eliangerardgg@gmail.com>
2024-04-22 22:35:27 -0600

    Add post length restriction

--- src/app/actions/add-post-action.ts
+++ src/app/actions/add-post-action.ts
@@ -5,16 +5,16 @@ import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { revalidatePath } from 'next/cache'
 
 export const addPost = async (formData: FormData) => {
-  const content = formData.get('content')
+  const content = formData.get('content')?.toString()
 
-  if (content === null) return
+  if (content === null || content === undefined || content.trim().length === 0) return
 
   const supabase = createServerActionClient({ cookies })
   // revisar si el usuario realmene está autentificado
   const { data: { user } } = await supabase.auth.getUser()
   if (user === null) return
 
-  await supabase.from('posts').insert({ content, user_id: user.id })
+  await supabase.from('posts').insert({ content: content.trim(), user_id: user.id })
 
   revalidatePath(`/?content=${content.toString()}`)
 }
